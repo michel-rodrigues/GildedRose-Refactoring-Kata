be5f0b4840a230e7ce4fc9789fcf5439ef8509df
Michel Rodrigues <michel.rodrigues86@yahoo.com.br>
2021-03-10 20:46:11 -0300

    change to single quotes

--- test_gilded_rose.py
+++ test_gilded_rose.py
@@ -6,10 +6,10 @@ from gilded_rose import Item, GildedRose
 
 class GildedRoseTest(unittest.TestCase):
     def test_foo(self):
-        items = [Item("foo", 0, 0)]
+        items = [Item('foo', 0, 0)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
-        self.assertEqual("fixme", items[0].name)
+        self.assertEqual('fixme', items[0].name)
 
 
 if __name__ == '__main__':
