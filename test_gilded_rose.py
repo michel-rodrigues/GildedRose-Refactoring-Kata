20a45edcb83374ef7f8ac4fc3a350c830755f0a6
Michel Rodrigues <michel.rodrigues86@yahoo.com.br>
2021-03-10 22:23:01 -0300

    setting up test cases

--- test_gilded_rose.py
+++ test_gilded_rose.py
@@ -5,11 +5,97 @@ from gilded_rose import Item, GildedRose
 
 
 class GildedRoseTest(unittest.TestCase):
-    def test_foo(self):
-        items = [Item('foo', 0, 0)]
-        gilded_rose = GildedRose(items)
+
+    def setUp(self):
+        self.quality = 12
+        self.sell_in = 20
+        self.item = Item('Regular Item', self.sell_in, self.quality)
+
+    def test_decrement_sell_in_by_one(self):
+        gilded_rose = GildedRose([self.item])
         gilded_rose.update_quality()
-        self.assertEqual('fixme', items[0].name)
+        self.assertEqual(self.item.sell_in, self.sell_in - 1)
+
+    def test_decrement_quality_by_one_before_sell_in_limit(self):
+        gilded_rose = GildedRose([self.item])
+        gilded_rose.update_quality()
+        self.assertEqual(self.item.quality, self.quality - 1)
+
+    def test_decrement_quality_by_two_after_sell_in_limit(self):
+        """When sell_in is less than 1."""
+        self.item.sell_in = 0
+        gilded_rose = GildedRose([self.item])
+        gilded_rose.update_quality()
+        self.assertEqual(self.item.quality, self.quality - 2)
+
+    def test_quality_should_not_be_decremented_less_than_zero(self):
+        self.item.quality = 0
+        gilded_rose = GildedRose([self.item])
+        gilded_rose.update_quality()
+        self.assertEqual(self.item.quality, 0)
+
+    @unittest.skip('NEED BUGFIX')
+    def test_aged_brie_quality_always_is_increased_by_one(self):
+        self.item.name = 'Aged Brie'
+        self.item.sell_in = 1
+        gilded_rose = GildedRose([self.item])
+        gilded_rose.update_quality()
+        self.assertEqual(self.item.quality, self.quality + 1)
+        self.assertEqual(self.item.sell_in, 0)
+        gilded_rose.update_quality()
+        self.assertEqual(self.item.quality, self.quality + 2)
+        self.assertEqual(self.item.sell_in, -1)
+        gilded_rose.update_quality()
+        self.assertEqual(self.item.quality, self.quality + 3)
+        self.assertEqual(self.item.sell_in, -2)
+
+    def test_quality_should_not_be_incremented_greather_than_fifty(self):
+        self.item.name = 'Aged Brie'
+        self.item.quality = 49
+        gilded_rose = GildedRose([self.item])
+        gilded_rose.update_quality()
+        self.assertEqual(self.item.quality, 50)
+        gilded_rose.update_quality()
+        self.assertEqual(self.item.quality, 50)
+
+    @unittest.skip('NEED BUGFIX')
+    def test_legendary_items_quality_should_not_be_updated(self):
+        self.item.name = 'Sulfuras'
+        self.item.sell_in = None
+        gilded_rose = GildedRose([self.item])
+        gilded_rose.update_quality()
+        self.assertEqual(self.item.quality, self.quality)
+
+    @unittest.skip('NEED BUGFIX :: Works just with full name')
+    def test_backstage_passes_quality_should_be_incremented_by_one_before_sell_in_limit(self):
+        self.item.name = 'Backstage passes'
+        gilded_rose = GildedRose([self.item])
+        gilded_rose.update_quality()
+        self.assertEqual(self.item.quality, self.quality + 1)
+
+    @unittest.skip('NEED BUGFIX :: Works just with full name')
+    def test_backstage_passes_quality_should_be_incremented_by_two_ten_days_before_sell_in_limit(self):
+        self.item.sell_in = 10
+        self.item.name = 'Backstage passes'
+        gilded_rose = GildedRose([self.item])
+        gilded_rose.update_quality()
+        self.assertEqual(self.item.quality, self.quality + 2)
+
+    @unittest.skip('NEED BUGFIX :: Works just with full name')
+    def test_backstage_passes_quality_should_be_incremented_by_two_five_days_before_sell_in_limit(self):
+        self.item.sell_in = 5
+        self.item.name = 'Backstage passes'
+        gilded_rose = GildedRose([self.item])
+        gilded_rose.update_quality()
+        self.assertEqual(self.item.quality, self.quality + 3)
+
+    @unittest.skip('NEED BUGFIX :: Works just with full name')
+    def test_backstage_passes_quality_should_be_zero_after_sell_in_limit(self):
+        self.item.sell_in = 0
+        self.item.name = 'Backstage passes'
+        gilded_rose = GildedRose([self.item])
+        gilded_rose.update_quality()
+        self.assertEqual(self.item.quality, 0)
 
 
 if __name__ == '__main__':
